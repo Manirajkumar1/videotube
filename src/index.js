f64b73710a512f5112697c5eae0180174d8829bd
Manirajkumar1 <manirajsingh7667@gmail.com>
2025-01-24 13:04:06 +0530

    utils custom Api

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ dotenv.config({
 
 connectDB()
 .then( () => {
-    app.listen(process.env.PORT || 8000
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server is running at port: ${process.env.PORT}`);
+        
+    }
         
     )
 })
